5f413bb8d6c2a1fb9a81c194c94b2f76b73524ca
Rayane <radharayg@gmail.com>
2020-10-16 20:49:58 -0300

    Commit - Entendendo propriedades

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
 
+interface TitleProps {
+  text: String;
+}
+
+function Title(props: TitleProps) {
+  return(
+    //<h1>Hello</h1>
+    <h1>{props.text}</h1>
+  )
+}
+
+//JSX - Javascript XML
+//é a forma de incluir HTML dentro de JS
+
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Title text="Titulo 1"/>
+      <Title text="Titulo 2"/>
+      <Title text="Titulo 3"/>
+      <Title text="Titulo 4"/>
     </div>
   );
 }
