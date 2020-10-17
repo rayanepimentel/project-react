2d1d86510fa218c8560abacd742cec629deb92fb
Rayane <radharayg@gmail.com>
2020-10-17 01:02:18 -0300

    Mcommit - map com leaflet

--- web/src/pages/MapOngs.tsx
+++ web/src/pages/MapOngs.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import logoLocal from '../img/logoLocal.svg';
 
 import '../styles/pages/map.css';
 
 function MapOngs() {
-    return(
-       <div id="page-map">
-           <aside>
-               <header>
-                   <img src={logoLocal} alt="Happy"></img>
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={logoLocal} alt="Happy"></img>
 
-                   <h2>Escolha uma ONG no mapa</h2>
-                   <p>Muitas crinças estão esperando sua doação :)</p>
-               </header>
+                    <h2>Escolha uma ONG no mapa</h2>
+                    <p>Muitas crinças estão esperando sua doação :)</p>
+                </header>
 
-               <footer>
-                   <strong>São Paulo</strong>
-                   <span>São Paulo</span>
-               </footer>
-           </aside>
+                <footer>
+                    <strong>São Paulo</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
 
-           <div></div>
+            <Map
+                center={[-23.5593829,-46.6412154]}
+                zoom={12}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
 
-           <Link to="" className="enter-ongs">
-               <FiPlus size={32} color="#FFF"/>
-           </Link>
-       </div>
+            <Link to="" className="enter-ongs">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     )
 }
 
