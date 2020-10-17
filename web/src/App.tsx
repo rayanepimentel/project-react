7fa017ef37959b3dbdfa790dadf76ba93e345928
Rayane <radharayg@gmail.com>
2020-10-16 22:23:45 -0300

    Commit - page landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -13,7 +13,8 @@ function App() {
 
         <main>
           <h1>Leve felicidade para o mundo</h1>
-          <p>Visite as ONGs e contribua com doações. Mude a vida de uma criança e de uma mãe.</p>
+          <p>Visite as ONGs e contribua com doações. <br/>
+            Mude a vida de uma criança e de uma mãe.</p>
         </main>
         
         <div className="location">
