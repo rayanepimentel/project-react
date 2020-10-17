32c8fec8829be46cb4c3b78e58f2483d6200183f
Rayane <radharayg@gmail.com>
2020-10-17 01:36:55 -0300

    commit - map mapbox

--- web/src/pages/MapOngs.tsx
+++ web/src/pages/MapOngs.tsx
@@ -32,6 +32,9 @@ function MapOngs() {
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                {/* <TileLayer 
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.MAP_TOKEN}`}
+                /> */}
             </Map>
 
             <Link to="" className="enter-ongs">
