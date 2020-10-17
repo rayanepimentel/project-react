5483ed3f51d559e260e30499298a2ab66957a924
Rayane <radharayg@gmail.com>
2020-10-16 23:34:40 -0300

    commit - page MapOng

--- web/src/pages/MapOngs.tsx
+++ web/src/pages/MapOngs.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import logoLocal from '../img/logoLocal.svg';
 
 function MapOngs() {
     return(
-        <h1>Hello</h1>
+       <div id="page-map">
+           <aside>
+               <header>
+                   <img src={logoLocal} alt="Happy"></img>
+
+                   <h2>Escolha uma ONG no mapa</h2>
+                   <p>Muitas crinças estão esperando sua doação.</p>
+               </header>
+
+               <footer>
+                   <strong>São Paulo</strong>
+                   <span>São Paulo</span>
+               </footer>
+           </aside>
+
+           <div></div>
+
+           <Link to="" className="create-ongs">
+               <FiPlus size={32} color="#FFF"/>
+           </Link>
+       </div>
     )
 }
 
