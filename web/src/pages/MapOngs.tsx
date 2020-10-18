f685e8e6b37e05190d70501641cd60ee57eb08de
Rayane <radharayg@gmail.com>
2020-10-18 13:57:54 -0300

    commit - listando ong no map

--- web/src/pages/MapOngs.tsx
+++ web/src/pages/MapOngs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
@@ -9,53 +9,74 @@ import logoLocal from '../img/logoLocal.svg';
 import mapIcon from '../utils/mapIcon';
 
 import '../styles/pages/map.css';
+import '../styles/pages/create-ong.css';
+import api from '../services/api';
 
+interface Ong {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 function MapOngs() {
-    return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={logoLocal} alt="Happy"></img>
+  const [ongs, setOngs] = useState<Ong[]>([]);
 
-                    <h2>Escolha uma ONG no mapa</h2>
-                    <p>Muitas crinças estão esperando sua doação :)</p>
-                </header>
+  useEffect(() => {
+    api.get('ongs').then(response => {
+      setOngs(response.data)
+    })
+  }, []);
 
-                <footer>
-                    <strong>São Paulo</strong>
-                    <span>São Paulo</span>
-                </footer>
-            </aside>
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={logoLocal} alt="Happy"></img>
 
-            <Map
-                center={[-23.5593829,-46.6412154]}
-                zoom={12}
-                style={{ width: '100%', height: '100%' }}
-            >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                {/* <TileLayer 
+          <h2>Escolha uma ONG no mapa</h2>
+          <p>Muitas crinças estão esperando sua doação :)</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-23.5593829, -46.6412154]}
+        zoom={12}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* <TileLayer 
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.MAP_TOKEN}`}
                 /> */}
 
-                <Marker
-                  icon={mapIcon}
-                  position={[-23.5593829,-46.6412154]}
-                >
-                    <Popup closeButton={false} minWidth={240} maxHeight={240} className="map-popup">
-                        nome da ONG
-                        <Link to="/ong/1">
-                            <FiArrowRight size={20} color="#FFF"/>
-                        </Link>
-                    </Popup>
-                </Marker>
-            </Map>
+        {ongs.map(ong => {
+          return (
+            <Marker
+              key={ong.id}
+              icon={mapIcon}
+              position={[ong.latitude, ong.longitude]}
+            >
+              <Popup closeButton={false} minWidth={240} maxHeight={240} className="map-popup">
+                {ong.name}
+                <Link to={`/ong/${ong.id}`}>
+                  <FiArrowRight size={20} color="#FFF" />
+                </Link>
+              </Popup>
+            </Marker>
+          )
+        })}
+      </Map>
 
-            <Link to="/ong/create" className="create-ong">
-                <FiPlus size={32} color="#FFF" />
-            </Link>
-        </div>
-    )
+      <Link to="/ong/create" className="create-ong">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  )
 }
 
 export default MapOngs;
