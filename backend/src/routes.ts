0ee1e23d87840106ac0c952b3976c2fb680c8d3a
Rayane <radharayg@gmail.com>
2020-10-17 21:00:03 -0300

    commit - upload de imagens

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OngsController from './controllers/OngsController'
 
 const routes = Router();
-
+const upload = multer(uploadConfig);
 
 routes.get('/ongs', OngsController.index);
 routes.get('/ongs/:id', OngsController.show);
-routes.post('/ongs', OngsController.create);
+routes.post('/ongs', upload.array('images'), OngsController.create);
 
 export default routes;
